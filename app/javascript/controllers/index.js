41f38bceda0cac8c16ef6c363086446f96996583
Justus Lambrecht <justuslambrecht@icloud.com>
2022-09-17 10:03:23 +0200

    update

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,17 @@
 
 import { application } from "./application"
 
-import TomSelectController from "./tom_select_controller.js"
+import JqueryBarRatingController from "./jquery_bar_rating_controller"
+application.register("jquery-bar-rating", JqueryBarRatingController)
+
+import Select2Controller from "./select2_controller"
+application.register("select2", Select2Controller)
+
+import StarRatingController from "./star_rating_controller"
+application.register("star-rating", StarRatingController)
+
+import TomSelectController from "./tom_select_controller"
 application.register("tom-select", TomSelectController)
 
-import StarRatingController from "./star_rating_controller.js"
-application.register("star-rating", StarRatingController)
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
